71e094b9025aaef7e46500932ae921b799b9a02d
Nafis Ahamed <nafisahamed14@gmail.com>
2024-05-12 04:27:05 +0600

    modal with pending

--- src/components/Assi2.jsx
+++ src/components/Assi2.jsx
@@ -3,6 +3,9 @@
 
 const Assi2 = ({item,items,setItems,ind}) => {
     console.log(items);
+    const handleSubmut = e=>{
+        e.preventDefault()
+    }
     const {image,title,description,status,marks,Submittedemail} = item
     return (
         <tbody>
@@ -11,7 +14,54 @@ const Assi2 = ({item,items,setItems,ind}) => {
           <td>{title}</td> 
           <td>{marks}</td> 
           <td>{Submittedemail}</td> 
-          <td> <button className="btn btn-warning">give mark</button></td> 
+          <td> <button
+            className="btn btn-primary"
+            onClick={() => document.getElementById("my_modal_5").showModal()}
+          >
+            give marks
+          </button>
+          <section>
+            <dialog
+              id="my_modal_5"
+              className="modal modal-bottom sm:modal-middle"
+            >
+              <div className="modal-box">
+                <h3 className="font-bold text-lg">Submission box</h3>
+                {/* <p className="py-4">
+                  Press ESC key or click the button below to close
+                </p> */}
+                <form onSubmit={handleSubmut}>
+                  <input
+                    type="text"
+                    placeholder="pdf link"
+                    className="input input-bordered w-full max-w-xs mt-5"
+                    name="link"
+                    required
+                  />
+                  <br />
+                  <textarea
+                    placeholder="quick note"
+                    className="textarea textarea-bordered textarea-sm w-full max-w-xs mt-5"
+                    name="box"
+                    required
+                  ></textarea>
+                  <input
+                    type="submit"
+                    className="lg:mt-4 md:mt-4 mt-2 btn btn-primary w-3/4 lg:ml-16"
+                    value="Submit"
+                  />
+                </form>
+                <div className="modal-action">
+                  <form method="dialog">
+                    {/* if there is a button in form, it will close the modal */}
+                    <button className="btn">Close</button>
+                  </form>
+                </div>
+              </div>
+            </dialog>
+          </section>
+
+          </td> 
         </tr>
        
       </tbody> 
