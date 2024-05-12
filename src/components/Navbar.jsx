f746f8d30d780822e195f62a3a1c33076385f955
Nafis Ahamed <nafisahamed14@gmail.com>
2024-05-12 22:56:27 +0600

    light night done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,29 @@
 import { Link, NavLink, Navigate } from "react-router-dom";
 import useAuth from "./useAuth";
 import { FaRegLightbulb } from "react-icons/fa6";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
   // const{users,logout} = useContext(AuthContext)
 
   const { users, logout } = useAuth();
-  const [mode,setMode] = useState(() => localStorage.getItem('lightmode') || 'light')
+  const [mode, setMode] = useState(
+    localStorage.getItem("lightmode") ? localStorage.getItem("lightmode") : "light"
+  );
 
-  const handlechange = () =>{
-    const oldmode = localStorage.getItem('lightmode')
-    const newMode = mode === 'light' ? 'dark' : 'light';
-    setMode(newMode);
-    localStorage.setItem('lightmode', newMode);
-  }
-  console.log(mode);
+  const handlechange = (e) => {
+    if (e.target.checked) {
+      setMode("dark");
+    } else {
+      setMode("light");
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem("lightmode", mode);
+    const localTheme = localStorage.getItem("lightmode");
+    document.querySelector("html").setAttribute("data-theme", localTheme);
+  }, [mode]);
 
   const handlelogout = () => {
     logout()
@@ -30,49 +38,72 @@ const Navbar = () => {
   };
   const links = (
     <>
-     <li> <input type="checkbox" onClick={handlechange} className="toggle toggle-success toggle-md mt-4 mr-2" /> </li>
       <li>
         {" "}
-        <Link className="mt-2" to="/">Home</Link>{" "}
+        <input
+          type="checkbox"
+          onClick={handlechange}
+          className="toggle toggle-success toggle-md mt-4 mr-2"
+        />{" "}
+      </li>
+      <li>
+        {" "}
+        <Link className="mt-2" to="/">
+          Home
+        </Link>{" "}
       </li>
 
-      <li> <Link className="mt-2" to="/assi">Assignment</Link> </li>
+      <li>
+        {" "}
+        <Link className="mt-2" to="/assi">
+          Assignment
+        </Link>{" "}
+      </li>
 
       {users?.email ? (
         <>
           <li>
             {" "}
-            <Link className="mt-2" to="/create">Create assignment</Link>{" "}
+            <Link className="mt-2" to="/create">
+              Create assignment
+            </Link>{" "}
           </li>
           <li>
             {" "}
-            <Link className="mt-2" to="/assi1">Pending assignment</Link>{" "}
+            <Link className="mt-2" to="/assi1">
+              Pending assignment
+            </Link>{" "}
           </li>
-      <li className="mt-2">
-        <details>
-          <summary className="">
-            user avatar
-          </summary>
-          <ul className="p-2 bg-base-100 rounded-t-none lg:p-8">
-            <Link to="/my"><span className="lg:p-8">My assignment attempt</span></Link>
-            
-          </ul>
-        </details>
-      </li>
+          <li className="mt-2">
+            <details>
+              <summary className="">user avatar</summary>
+              <ul className="p-2 bg-base-100 rounded-t-none lg:p-8">
+                <Link to="/my">
+                  <span className="lg:p-8">My assignment attempt</span>
+                </Link>
+              </ul>
+            </details>
+          </li>
           <li>
             {" "}
-            <button className="mt-2" onClick={handlelogout}>Log out</button>{" "}
+            <button className="mt-2" onClick={handlelogout}>
+              Log out
+            </button>{" "}
           </li>{" "}
         </>
       ) : (
         <>
           {" "}
           <li>
-            <NavLink className="mt-2" to="/login">Login</NavLink>
+            <NavLink className="mt-2" to="/login">
+              Login
+            </NavLink>
           </li>{" "}
           <li>
             {" "}
-            <Link className="mt-2" to="/registration">Sign Up</Link>{" "}
+            <Link className="mt-2" to="/registration">
+              Sign Up
+            </Link>{" "}
           </li>{" "}
         </>
       )}
@@ -105,7 +136,11 @@ const Navbar = () => {
             {links}
           </ul>
         </div>
-        <img className="h-24 w-24 ml-4" src="https://i.postimg.cc/tCYs3Ygp/302424953-491546976311439-2706882930290413380-n.png" alt="" />
+        <img
+          className="h-24 w-24 ml-4"
+          src="https://i.postimg.cc/tCYs3Ygp/302424953-491546976311439-2706882930290413380-n.png"
+          alt=""
+        />
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{links}</ul>
